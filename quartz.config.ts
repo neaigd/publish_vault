3b94ded1f53d05932dc81607c817a4b9c766c483
Luiz Peixoto <peixoto@riseup.net>
2025-03-17 11:48:51 -0300

    Quartz sync: Mar 17, 2025, 11:48 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
+    pageTitle: "Núcleo de Estudos",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
